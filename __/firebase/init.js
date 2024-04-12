2f1fe4a8f36438d62ed31b978bfbb0382ebc6ec6
Brianceasar <95188858+Brianceasar@users.noreply.github.com>
2024-04-12 13:19:54 +0300

    inital commit

---
+++ __/firebase/init.js
@@ -0,0 +1,9 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyCXUreez1JuMc51TzUS3AX1fIyzRzO2XSM",
+  "authDomain": "imperium-opes.firebaseapp.com",
+  "databaseURL": "",
+  "messagingSenderId": "770841764927",
+  "projectId": "imperium-opes",
+  "storageBucket": "imperium-opes.appspot.com"
+});
